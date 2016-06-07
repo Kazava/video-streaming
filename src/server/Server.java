1623b4fec79674b8f13136f0e21de46a761e5a54
Thilo Ilg <contact@thiloilg.com>
2016-06-07 19:46:00 +0200

    server response with html file

---
+++ src/server/Server.java
@@ -0,0 +1,62 @@
+package server;
+
+import java.io.*;
+import java.net.*;
+import com.sun.net.httpserver.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Server {
+
+    public static void main(String[] args) {
+        try {
+            HttpServer httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
+            httpServer.createContext("/", new MyHttpHandler());
+            httpServer.setExecutor(null);
+            httpServer.start();
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    static class MyHttpHandler implements HttpHandler{
+
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String root = "/res";
+            URI uri = t.getRequestURI();
+            log(uri.getPath());
+            File file = new File("res/index.html").getCanonicalFile();
+            if (!file.isFile()) {
+            	// Object does not exist or is not a file: reject with 404 error.
+            	String response = "404 (Not Found)\n";
+            	t.sendResponseHeaders(404,response.length());
+            	OutputStream os = t.getResponseBody();
+            	os.write(response.getBytes());
+            	os.close();
+            } else {
+            	// Object exists and is a file: accept with response code 200.
+            	t.sendResponseHeaders(200, 0);
+            	OutputStream os = t.getResponseBody();
+            	FileInputStream fs = new FileInputStream(file);
+            	final byte[] buffer = new byte[0x10000];
+            	int count = 0;
+            	while ((count = fs.read(buffer)) >= 0) {
+            		os.write(buffer,0,count);
+            	}
+            	fs.close();
+            	os.close();
+          	}
+        }
+    }
+    
+    public static void log(String str){
+    	System.out.println(str);
+    }
+    
+}
