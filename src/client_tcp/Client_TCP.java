8728c3b6202ab6e0ca121fc06cb31f7cf59f0329
LesserCat <kalli.t@hotmail.com>
2016-06-14 11:21:48 +0200

    Add not working udp functionality

--- src/client_tcp/Client_TCP.java
+++ src/client_tcp/Client_TCP.java
@@ -8,32 +8,101 @@ import java.net.*;
 public class Client_TCP {
 	public static void main(String argv[]) throws Exception
     {
+		PrintStream ps = null;
+		Scanner scan = null;
+		InputStreamReader isr = null;
+		BufferedReader br = null;
+		Socket clientSocketTCP = null;
+		DatagramSocket clientSocketUDP = null;
+		
 		while (true) {
-	        Socket clientSocket = new Socket("localhost", 8001);
-	        
+			debug("Choose connection between TCP and UDP: ");
+	        scan = new Scanner(System.in);
+	        String choice = scan.nextLine();
+	        if (choice.equals("TCP"))
+	        	clientSocketTCP = new Socket("localhost", 8001);
+	        else if (choice.equals("UDP")) {	        	 
+	            InetAddress IPAddress = InetAddress.getByName("localhost");
+	        	clientSocketUDP = new DatagramSocket(8001, IPAddress);
+	        }
+	        String clientCommand = "";
+	        clientCommand = openConnections(choice, ps, scan, isr, br, clientSocketTCP, clientSocketUDP);
+	        /*
 	        //Sends message to the server
-	        PrintStream ps = new PrintStream(clientSocket.getOutputStream());
-	        Scanner scan = new Scanner(System.in);
+	        ps = new PrintStream(clientSocketTCP.getOutputStream());
+	        scan = new Scanner(System.in);
 	        String cMessage ="";
-	        InputStreamReader ir = new InputStreamReader(clientSocket.getInputStream());
-	        BufferedReader br = new BufferedReader(ir);
+	        isr = new InputStreamReader(clientSocketTCP.getInputStream());
+	        br = new BufferedReader(isr);
 		    cMessage = scan.nextLine();
 		    ps.println(cMessage);
+		    
 		    
 		    //Reads and displays response from server
 		    String message = br.readLine().trim();
 		    System.out.println(message);
+		    */
 		    
 		    // End connection by typing "END"
-	        if (cMessage.trim().equals("END")) {
-		        System.out.println("Closing connections");
-		        br.close();
-		        ir.close();
-		        scan.close();
-		        ps.close();
-		        clientSocket.close();
+	        
+	        if (clientCommand.equals("END")) {
+		        closeConnections(ps, scan, isr, br);
+		        clientSocketTCP.close();
 		        return;
 	        }
+	        if (clientCommand.trim().equals("UDP")) {
+	        	// Close connection to switch from TCP to UDP:
+		        closeConnections(ps, scan, isr, br);
+		        clientSocketUDP.close();
+		        // Open UDP socket: 
+
+	        }
         }
+		
     }
+	
+	public static String openConnections(String socketType, PrintStream ps, Scanner scan, InputStreamReader isr, 
+								BufferedReader br, Socket tcp, DatagramSocket udp) throws IOException {
+		debug("opening connections");
+		if (socketType.equals("TCP")) {
+			ps = new PrintStream(tcp.getOutputStream());
+			debug("Write anything");
+			Scanner sc = new Scanner(System.in);
+			String cMessage = "";
+		    cMessage = scan.nextLine();
+			isr = new InputStreamReader(tcp.getInputStream());
+			br = new BufferedReader(isr);
+		    ps.println(cMessage);
+		    // Print response:
+		    String message = br.readLine().trim();
+		    System.out.println(message);
+		    return message;
+		}
+		if (socketType.equals("UDP")) {
+		    byte[] receiveData = new byte[1024];
+		    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+		    udp.receive(receivePacket);
+		    debug("Received udp-packet");
+		    return "upd";
+		}
+		return "fail";
+		
+	}
+	public static void closeConnections(PrintStream ps, Scanner scan, InputStreamReader isr, 
+										BufferedReader br) throws IOException {
+        System.out.println("Closing connections");
+		if (ps != null)
+			ps.close();
+		if (scan != null)
+			scan.close();
+		if (isr != null)
+			isr.close();
+		if (br != null)
+			br.close();
+	}
+	
+	  // Because "System.out.println" is too long to type...
+	public static void debug(String str){
+	  	System.out.println(str);
+	}
 }
