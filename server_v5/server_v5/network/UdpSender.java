cd26ce249fac4ea415db664325e1fa515ad2d11b
CarlosCarlos <kalli.t@hotmail.com>
2016-07-11 22:41:47 +0200

    * Cleaned class UdpSender *

--- server_v5/server_v5/network/UdpSender.java
+++ server_v5/server_v5/network/UdpSender.java
@@ -1,13 +1,10 @@
 package server_v5.network;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 
-import server_v5.video.Frame;
 import server_v5.video.Video;
 
 public class UdpSender implements Runnable {
@@ -26,48 +23,34 @@ public class UdpSender implements Runnable {
 	public void sendUdp(){
 		try {
 			udpChannel = DatagramChannel.open();
-			//udpChannel.connect(new InetSocketAddress("localhost", 9999));
 			buffer = ByteBuffer.allocate(video.getByteSizeOfFrame()); // The new buffer's capacity, in bytes
 			
 			int size = video.getFramesSize();
 			int[] pixels;
 			
-			/** set header **/
-//			buffer.putInt(115);
-//			buffer.putInt(70);
-			
 			while(0 < video.getFrames(1)){
 				buffer.clear();
-				
 				pixels = video.getFrames().poll().getPixels();
-				
 				for(int i = 0; i < size; i++){
 					buffer.putInt(pixels[i]);
 				}
-				
-
 				buffer.flip();
-				
-				//udpChannel.write(buffer);
 				udpChannel.send(buffer, new InetSocketAddress("localhost", 9999));
-//				printBytes(buffer.array());
-				
+//				printBytes(buffer.array()); // for debugging
 				try {
 					Thread.sleep(32);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-			
 			udpChannel.close();
 			System.out.println("Server finished sending video.");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	// For Debugging ByteBuffer:
 	private void printBytes(byte[] in){
 		System.out.println(" *** Bytes from Server ***");
 		for(int i = 0; i < in.length; i++){
