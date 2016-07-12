91604a0b8b057d28dbdd49a571a4a59ce5171bcd
Thilo Ilg <contact@thiloilg.com>
2016-07-12 02:09:34 +0200

    subsampling added

--- client_v5/client_v5/compression/Compression.java
+++ client_v5/client_v5/compression/Compression.java
@@ -7,9 +7,19 @@ public class Compression {
 		
 		// TODO: decode chroma subsampling
 		
+		int[] temp = subsampling(pixels);
 		
-		
-		return pixels;
+		return temp;
+	}
+
+	private static int[] subsampling(int[] pixels) {
+		int factor = 2;
+		int length = pixels.length * factor;
+		int[] result = new int [length];
+		for(int i = 0; i < length; i++){
+			result[i] = pixels[i / factor];
+		}
+		return result;
 	}
 	
 }
--- server_v5/server_v5/compression/Compression.java
+++ server_v5/server_v5/compression/Compression.java
@@ -5,11 +5,33 @@ public class Compression {
 	public static int[] encode(int[] pixels) {
 		
 		
+		int[] temp = subsampling(pixels);
+		
+		return temp;
+	}
+
+	/**
+	 * subsampling of pixels by given factor
+	 * @param pixels
+	 * @return
+	 */
+	private static int[] subsampling(int[] pixels) {
+		int factor = 2;
+		int length = pixels.length / factor;
+		int[] result = new int[length];
+		
+		for(int i = 0; i < length; i++){
+			result[i] = pixels[i * factor];
+		}
+		return result;
+	}
+
+	private static int[] chromaSubsampling(int[] pixels) {
+		
+		
 		//TODO: Chroma subsampling
 		
-		
-		
-		return pixels;
+		return null;
 	}
 
 }
